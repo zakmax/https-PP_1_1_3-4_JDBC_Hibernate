0fcdc4ab3c5498cc837a183be1284427870bf5e3
zak-max15@yandex.ru <java180490>
2025-04-30 18:12:27 +0300

    2е доисправление задачи 1.1.4 после проверки Ментором

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,5 +1,6 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.service.UserService;
 import jm.task.core.jdbc.service.UserServiceImpl;
 
 import static jm.task.core.jdbc.util.Util.getSessionFactory;
@@ -8,17 +9,17 @@ public class Main {
     public static void main(String[] args) throws Exception {
 
 
-        UserServiceImpl userDaoHibernate = new UserServiceImpl();
+        UserService userService = new UserServiceImpl();
         try {
-            userDaoHibernate.createUsersTable();
-            userDaoHibernate.saveUser("Max", "Yuha", (byte) 34);
-            userDaoHibernate.saveUser("Max2", "Yuha2", (byte) 35);
-            userDaoHibernate.saveUser("Max3", "Yuha3", (byte) 36);
-            userDaoHibernate.saveUser("Max4", "Yuha4", (byte) 37);
-            userDaoHibernate.removeUserById(1);
-            userDaoHibernate.getAllUsers();
-            userDaoHibernate.cleanUsersTable();
-            userDaoHibernate.dropUsersTable();
+            userService.createUsersTable();
+            userService.saveUser("Max", "Yuha", (byte) 34);
+            userService.saveUser("Max2", "Yuha2", (byte) 35);
+            userService.saveUser("Max3", "Yuha3", (byte) 36);
+            userService.saveUser("Max4", "Yuha4", (byte) 37);
+            userService.removeUserById(1);
+            userService.getAllUsers();
+            userService.cleanUsersTable();
+            userService.dropUsersTable();
         } finally {
             getSessionFactory().close();
         }
